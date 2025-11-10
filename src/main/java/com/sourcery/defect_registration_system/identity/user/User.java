4426c250dcaeac54684d421ecabd5b8046cb5193
Vitalijus Rusakevic <vitalijus.rusakevic@vandnesparkas.lt>
2025-11-10 18:31:41 +0200

    Refactor: replace Long with UUID.

--- src/main/java/com/sourcery/defect_registration_system/identity/user/User.java
+++ src/main/java/com/sourcery/defect_registration_system/identity/user/User.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -14,6 +13,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
 
 @Entity
 @Table(name = "users")
@@ -26,8 +28,9 @@ import lombok.ToString;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue
+    @UuidGenerator
+    private UUID id;
 
     @Column(nullable = false)
     private String name;
