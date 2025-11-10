365635d62271c70842dfe8da1ce504d260b97c15
Vitalijus Rusakevic <vitalijus.rusakevic@vandnesparkas.lt>
2025-11-10 21:54:33 +0200

    fix UserRepository add UUID

--- src/main/java/com/sourcery/defect_registration_system/identity/user/User.java
+++ src/main/java/com/sourcery/defect_registration_system/identity/user/User.java
@@ -42,5 +42,5 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    private String image;
+    private String imageUrl;
 }
--- src/main/java/com/sourcery/defect_registration_system/repository/UserRepository.java
+++ src/main/java/com/sourcery/defect_registration_system/repository/UserRepository.java
@@ -4,8 +4,10 @@ import com.sourcery.defect_registration_system.identity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
+
 
     Optional<User> findByEmail(String email);
 }
