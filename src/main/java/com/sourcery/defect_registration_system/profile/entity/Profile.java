cf58a8bcbfd4088f161d66de76f85572b70d15c5
unknown <pratikthakur187@gmail.com>
2025-12-09 18:06:20 +0200

    Adding local files

---
+++ src/main/java/com/sourcery/defect_registration_system/profile/entity/Profile.java
@@ -0,0 +1,61 @@
+package com.sourcery.defect_registration_system.profile.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "user_profile", uniqueConstraints = {@UniqueConstraint(columnNames = "user_id")})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Profile {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false, unique = true, updatable = false)
+    private UUID userId;
+
+    @Column(name = "department", length = 100)
+    private String department;
+
+    @Column(name = "role", length = 50)
+    private String role;
+
+    @Column(name = "street_address", length = 255)
+    private String streetAddress;
+
+    @Column(name = "city", length = 100)
+    private String city;
+
+    @Column(name = "state_province", length = 100)
+    private String stateProvince;
+
+    @Column(name = "postcode", length = 20)
+    private String postcode;
+
+    @Column(name = "country", length = 50)
+    private String country;
+
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    @PrePersist
+    @PreUpdate
+    public void setUpdatedAt() {
+        this.updatedAt = Instant.now();
+    }
+}
