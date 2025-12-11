4a1a68bd3a90405271b048bff120e82bb6e0f003
unknown <pratikthakur187@gmail.com>
2025-12-11 13:33:56 +0200

    Remove unwanted files from PR

--- src/main/java/com/sourcery/defect_registration_system/profile/exceptions/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.sourcery.defect_registration_system.profile.exceptions;
-
-import java.util.UUID;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(UUID userId) {
-        super("User not found with userId: " + userId);
-    }
-}
