7adc304ab70d77f07160ae01a798ed6f53e62300
Kostasz75 <kostas.zibulskis@gmail.com>
2025-12-17 11:41:13 +0200

    Replaced * import with specific imports

--- src/main/java/com/sourcery/defect_registration_system/issue/repository/IssueRepository.java
+++ src/main/java/com/sourcery/defect_registration_system/issue/repository/IssueRepository.java
@@ -3,7 +3,13 @@ package com.sourcery.defect_registration_system.issue.repository;
 import com.sourcery.defect_registration_system.issue.dto.UpdateIssueRequest;
 import com.sourcery.defect_registration_system.issue.entity.Issue;
 import com.sourcery.defect_registration_system.issue.enums.IssueStatus;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.Delete;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
