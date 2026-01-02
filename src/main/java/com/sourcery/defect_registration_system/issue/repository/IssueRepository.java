4e3b83699aeebd5f836eebe28149329cf6879a2a
Kostasz75 <kostas.zibulskis@gmail.com>
2026-01-02 22:08:39 +0200

    Deleted counting votes in the issueRepository in getIssueById

--- src/main/java/com/sourcery/defect_registration_system/issue/repository/IssueRepository.java
+++ src/main/java/com/sourcery/defect_registration_system/issue/repository/IssueRepository.java
@@ -85,20 +85,16 @@ public interface IssueRepository {
 
     @Select("""
     SELECT
-      i.id              AS id,
-      i.summary         AS summary,
-      i.description     AS description,
-      i.office_id       AS officeId,
-      i.status          AS status,
-      i.created_by      AS createdBy,
-      i.date_created    AS dateCreated,
-      i.date_modified   AS dateModified,
-      COUNT(iv.user_id) AS voteCount
-    FROM issue i
-    LEFT JOIN issue_vote iv ON iv.issue_id = i.id
-    WHERE i.id = #{id}
-    GROUP BY
-      i.id, i.summary, i.description, i.office_id, i.status, i.created_by, i.date_created, i.date_modified
+      id,
+      summary,
+      description,
+      office_id    AS officeId,
+      status,
+      created_by  AS createdBy,
+      date_created AS dateCreated,
+      date_modified AS dateModified
+    FROM issue
+    WHERE id = #{id}
     """)
     Optional<Issue> getIssueById(@Param("id") UUID id);
 
