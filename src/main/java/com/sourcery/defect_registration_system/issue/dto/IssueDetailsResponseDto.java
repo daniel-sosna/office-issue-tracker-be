b38b3ac1d6d89103e27c6e536a66980da07319b1
Daniel Sosna <danik.sosna@gmail.com>
2026-01-09 01:42:22 +0200

    Forbid user to vote on his own issues

--- src/main/java/com/sourcery/defect_registration_system/issue/dto/IssueDetailsResponseDto.java
+++ src/main/java/com/sourcery/defect_registration_system/issue/dto/IssueDetailsResponseDto.java
@@ -2,7 +2,6 @@ package com.sourcery.defect_registration_system.issue.dto;
 
 import com.sourcery.defect_registration_system.attachment.dto.IssueAttachmentResponse;
 
-import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
 
