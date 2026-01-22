fe23804f2cec57a047ffaadaf3aec3e6bad47211
unknown <pratikthakur187@gmail.com>
2026-01-22 20:33:56 +0200

    Removing unwanted code

--- src/main/java/com/sourcery/defect_registration_system/issue_vote/service/VoteService.java
+++ src/main/java/com/sourcery/defect_registration_system/issue_vote/service/VoteService.java
@@ -1,7 +1,5 @@
 package com.sourcery.defect_registration_system.issue_vote.service;
 
-import com.sourcery.defect_registration_system.issue.entity.Issue;
-import com.sourcery.defect_registration_system.issue.exceptions.IssueNotFoundException;
 import com.sourcery.defect_registration_system.exception.BadRequestException;
 import com.sourcery.defect_registration_system.issue.repository.IssueRepository;
 import com.sourcery.defect_registration_system.issue_vote.dto.IssueVoteCountDto;
@@ -17,13 +15,13 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
-import java.util.Collections;
 import java.util.stream.Collectors;
 
 @Service
@@ -69,11 +67,6 @@ public class VoteService {
                 .build();
 
         voteRepository.insertVote(vote);
-
-        Issue issue = issueRepository.getIssueById(issueId)
-                .orElseThrow(() -> new IssueNotFoundException("Issue with ID " + issueId + " not found"));
-        UUID issueReporterId = issue.getCreatedBy();
-        String upvotedBy = authService.getCurrentUserInfo(principal).name();
 
         return VoteResponseDto.from(vote);
     }
